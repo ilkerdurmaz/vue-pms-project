4143b6b9e75bd3e5da4cfd54d8182ab74a8c8b29
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2022-10-05 21:42:10 +0300

    add CardCounter component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-
+import CardCounter from './components/Card/CardCounter.vue'
 </script>
 
 <template>
-	<div>
 
+	<div class="counter-group">
+		<CardCounter :data="{number : 14, name : 'Task'}"></CardCounter>
+		<CardCounter :data="{number : 1, name : 'Members'}"></CardCounter>
 	</div>
+
+
 </template>
 
-<style scoped>
+<style lang="scss">
+.counter-group {
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: center;
+}
+
 
 </style>
---
+++ src/components/Card/CardCounter.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps(['data']);
+</script>
+        
+<template>
+    <div class="counter">
+        <div class="counter__header">
+            {{ props.data.number }}
+        </div>
+        <div class="counter__content">
+            {{ props.data.name }}
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.counter {
+ 
+    // position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 20px;
+    background: #F9F9F9;
+    border-radius: 7px;
+    width: fit-content;
+    gap: 4px;
+    width: 74px;
+
+    &__header {
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 21px;
+        color: #3C557A;
+    }
+
+    &__content {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        color: #717986;
+    }
+}
+</style>
