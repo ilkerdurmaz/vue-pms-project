5147840e2a45f58daa1a7adb6a97b4fce0482fb6
Esra Koçak <esrakocak470@gmail.com>
2022-10-05 22:34:28 +0300

    sidebar added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,55 @@
 <script setup>
-
-</script>
-
-<template>
-	<div>
-
-	</div>
-</template>
-
-<style scoped>
-
-</style>
+	import Sidebar from './components/sidebar.vue'
+	
+	</script>
+	
+	<template>
+			<div class="app">
+				 <Sidebar />
+			</div>
+	</template>
+	
+	<style lang="scss">
+	:root {
+	--primary: #00db99;
+		--primary-alt: #00db99;
+		--dark: #717986;
+		--dark-alt: #969696;
+		--light: #ffffff;
+		--red: #FF285C;
+		--sidebar-width: 200px;
+	}
+	
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: 'Roboto', sans-serif;
+	}
+	
+	body {
+		background: var(--light);
+	}
+	
+	button {
+		cursor: pointer;
+		appearance: none;
+		border: none;
+		background: none;
+	}
+	
+	.app {
+		display: flex;
+	
+		main {
+			flex: 1 1 0;
+			padding: 2rem;
+	
+			@media (max-width: 768px) {
+				padding-left: 6rem;
+				
+			}
+		}
+	}
+	</style>
+	
