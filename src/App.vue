f2d5ae0fb585eede89ff4046aca906e184ff2a5d
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-10-05 23:31:55 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
 	import Sidebar from './components/sidebar.vue'
 	
-	</script>
+</script>
 	
 	<template>
 			<div class="app">
 				 <Sidebar />
 			</div>
 	</template>
+
+
+<style lang="scss" >
 	
-	<style lang="scss">
+	.app {
+		display: flex;
+	
+		main {
+			flex: 1 1 0;
+			padding: 2rem;
+	
+			@media (max-width: 768px) {
+				padding-left: 6rem;
+				
+			}
+		}
+	}
+
 	:root {
 	--primary: #00db99;
 		--primary-alt: #00db99;
@@ -37,54 +53,4 @@
 		border: none;
 		background: none;
 	}
-	
-	.app {
-		display: flex;
-	
-		main {
-			flex: 1 1 0;
-			padding: 2rem;
-	
-			@media (max-width: 768px) {
-				padding-left: 6rem;
-				
-			}
-		}
-	}
-	</style>
-	
-import Card from './components/Card/Card.vue';
-
-</script>
-
-<template>
-	<div>
-		<Card />
-	</div>
-</template>
-
-<style >
-.flex {
-	display: flex;
-}
-
-.flex-column {
-	flex-direction: column;
-}
-
-.flex-row {
-	flex-direction: row;
-}
-
-.justify-between {
-	justify-content: space-between;
-}
-
-.justify-center {
-	justify-content: center;
-}
-
-.items-center {
-	align-items: center;
-}
 </style>
